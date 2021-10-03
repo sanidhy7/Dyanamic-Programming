925cedf3920561a3c93581bce1d0970b81c72d9e
AYUSH RANJAN TRIPATHI <73754672+ayushrtripati143@users.noreply.github.com>
2021-10-03 10:37:12 +0530

    Minimum no of ways to cover distance

---
+++ Minimum no of ways to cover distance.py
@@ -0,0 +1,15 @@
+def printCountRec(dist):
+	
+	# Base cases
+	if dist < 0:
+		return 0
+		
+	if dist == 0:
+		return 1
+
+	return (printCountRec(dist-1) +
+			printCountRec(dist-2) +
+			printCountRec(dist-3))
+
+dist = 4
+print(printCountRec(dist))
